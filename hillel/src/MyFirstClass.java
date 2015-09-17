c3e4a769675e4cc8ee4ae2e6dc82f58ed1be15dd
APavlovF <pavlav82@ukr.net>
2015-09-17 21:20:18 +0300

    helo world commit

---
+++ hillel/src/MyFirstClass.java
@@ -0,0 +1,21 @@
+/**
+ * Created by APavlov on 17.09.2015.
+ */
+public class MyFirstClass {
+    public static void main(String[] args) {
+        String helloMessage = "Hello world";
+        System.out.println(helloMessage);
+
+        final int maxBirthYear = 1982;
+
+        int age = 33;
+
+        //maxBirthYear = 2015;
+
+        System.out.println("I'm " + age);
+
+        System.out.println("31" + age);
+
+        System.out.println(31 + age);
+    }
+}
