c3b861fd8229604aafe38bec30788a03712c6bf4
APavlovF <pavlav82@ukr.net>
2015-10-04 21:08:14 +0300

    calculator

--- hillel/src/Crypt.java
+++ hillel/src/Crypt.java
@@ -1,19 +1,22 @@
 public class Crypt {
     public static void main(String[] args) {
-        char key = '7'; //crypt
+        char key = '1'; //crypt
         char[] chars = "someText".toCharArray();
         System.out.println("Crypted message: ");
 
-        for (int i = 0; i < chars.length; i++) {
-            chars[i] ^= key;
-            System.out.print(chars[i]);
-        }
-        System.out.println();
+        crypt(key, chars);
+        String cryptedString = String.copyValueOf(chars);
+        System.out.println(cryptedString);
+
         System.out.println("Uncrypted message: ");
-        for (int i = 0; i < chars.length; i++) {
-            chars[i] ^= key;
-        }
+        crypt(key, chars);
         String someString = String.copyValueOf(chars);
         System.out.println(someString);
+    }
+
+    private static void crypt(char key, char[] chars) {
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] ^= key;
+        }
     }
 }
